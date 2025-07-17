9b2a17ec141f8503c6e813399f63000540fa16ae
Muthubalas <muthubala894@gmail.com>
2025-07-17 12:54:16 +0530

    tags updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../assets/css/home.css';
 import logo from '../assets/images/logo.png';
+import franchise from '../assets/images/ariha_ franchise.pdf';
+import catelog from '../assets/images/ariha_catlg.pdf';
 import { Row, Col, Container, Button } from 'react-bootstrap';
 import fb from '../assets/images/logos_facebook.png';
 import insta from '../assets/images/skill-icons_instagram.png';
@@ -25,12 +27,13 @@ function Footer() {
               </p>
               <div className="d-flex flex-column flex-md-row gap-2 justify-content-center justify-content-md-start">
                 <Button className='yellowbutton'>
-                  <MdOutlineFileDownload className="me-2" />
-                  Be Our Franchisee
+               <a href={catelog}  ><MdOutlineFileDownload className="me-2" />
+                  Be Our Franchisee</a>   
                 </Button>
                 <Button className='yellowbutton'>
-                  <MdOutlineFileDownload className="me-2" />
-                  Download Brochure
+                  <a href={franchise}>  <MdOutlineFileDownload className="me-2" />
+                  Download Brochure</a>
+                
                 </Button>
               </div>
             </Col>
@@ -47,20 +50,20 @@ function Footer() {
             {/* Categories */}
             <Col xs={12} md={2} className="text-center text-md-start mb-4 mb-md-0">
               <h5 className='fw-semibold pb-2 text-uppercase'>Categories</h5>
-              <p><Link to="/">Brownies</Link></p>
-              <p><Link to="/service">Buns & Loaves</Link></p>
-              <p><Link to="/contact">Muffins</Link></p>
-              <p><Link to="/contact">Pizza Bases</Link></p>
+              <p><Link to="/shop">Brownies</Link></p>
+              <p><Link to="/shop">Buns & Loaves</Link></p>
+              <p><Link to="/shop">Muffins</Link></p>
+              <p><Link to="/shop">Pizza Bases</Link></p>
             </Col>
 
             {/* Get in Touch */}
             <Col xs={12} md={4} className="text-center text-md-start">
               <h5 className='fw-semibold pb-2 text-uppercase'>Get in Touch</h5>
               <div className='d-flex justify-content-center justify-content-md-start pb-2'>
-                <LuPhone size={24} className='me-2' /> +91 98844 55605
+                <LuPhone size={24} className='me-2' /> +91 9884455605
               </div>
               <div className='d-flex justify-content-center justify-content-md-start pb-2'>
-                <CiMail size={24} className='me-2' /> arihafoods@gmail.com
+                <CiMail size={24} className='me-2' /> wecare@arihafoods.com
               </div>
               <div className='d-flex justify-content-center justify-content-md-start pb-2 text-center text-md-start'>
                 <LuMapPinCheckInside size={24} className='me-2' /> MIG 156B, 6th Main Rd,<br />
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,198 +1,14 @@
-// import React, { useState } from 'react';
-// import { Col, Container, Form, Row } from 'react-bootstrap';
-// import * as emailjs from '@emailjs/browser'; 
-// import image from '../assets/images/photo.webp';
-// import { LuPhone } from "react-icons/lu";
-
-// function Contact() {
-//   const [formData, setFormData] = useState({
-//     name: '',
-//     phone: '',
-//     email: '',
-//     service: '',
-//     message: '',
-//   });
-
-//   const [validated, setValidated] = useState(false);
-//   const [sending, setSending] = useState(false);
-//   const [sent, setSent] = useState(false);
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData((prev) => ({ ...prev, [name]: value }));
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     const form = e.currentTarget;
-
-//     if (form.checkValidity() === false) {
-//       e.stopPropagation();
-//       setValidated(true);
-//       return;
-//     }
-
-//     setValidated(true);
-//     setSending(true);
-
-//     emailjs
-//       .send(
-//         'service_r2h273n',
-//         'template_nhoy05f',
-//         formData,
-//         'cTHXHzBfD_w8wvixJ'
-//       )
-//       .then(() => {
-//         setSent(true);
-//         setSending(false);
-//         alert('Message sent successfully!');
-//         setFormData({ name: '', phone: '', email: '', service: '', message: '' });
-//         setValidated(false);
-//         console.log('Sending data:', formData);
-//       })
-//       .catch((error) => {
-//         setSending(false);
-//         alert('Failed to send. Please try again.');
-//         console.error(error);
-//       });
-//   };
-
-//   return (
-//     <Container className="text-center my-5">
-//       <h1>Contact us</h1>
-//       <div className="form my-5">
-//         <Form noValidate validated={validated} onSubmit={handleSubmit} className="text-start">
-//           <Row>
-//             <Col md={6} xs={12}>
-//               <Form.Group className="mb-3">
-//                 <Form.Label className="pb-3">Full Name*</Form.Label>
-//                 <Form.Control
-//                   type="text"
-//                   name="name"
-//                   value={formData.name}
-//                   onChange={handleChange}
-//                   placeholder="Your Name"
-//                   required
-//                 />
-//               </Form.Group>
-//             </Col>
-//             <Col md={6} xs={12}>
-//               <Form.Group className="mb-3">
-//                 <Form.Label className="pb-3">Email*</Form.Label>
-//                 <Form.Control
-//                   type="email"
-//                   name="email"
-//                   value={formData.email}
-//                   onChange={handleChange}
-//                   placeholder="name@example.com"
-//                   required
-//                 />
-//               </Form.Group>
-//             </Col>
-//           </Row>
-//           <Row>
-//             <Col md={6} xs={12}>
-//               <Form.Group className="mb-3">
-//                 <Form.Label className="pb-3">Phone number*</Form.Label>
-//                 <Form.Control
-//                   type="tel"
-//                   name="phone"
-//                   value={formData.phone}
-//                   onChange={handleChange}
-//                   placeholder="Your Phone no"
-//                   required
-//                 />
-//               </Form.Group>
-//             </Col>
-//             <Col md={6} xs={12}>
-//               <Form.Group className="mb-3">
-//                 <Form.Label className="pb-3">Product name</Form.Label>
-//                 <Form.Select
-//                   name="service"
-//                   value={formData.service}
-//                   onChange={handleChange}
-//                   required
-//                 >
-//                   <option value="">Open this select menu</option>
-//                   <option value="1">One</option>
-//                   <option value="2">Two</option>
-//                   <option value="3">Three</option>
-//                 </Form.Select>
-//               </Form.Group>
-//             </Col>
-//           </Row>
-//           <Row>
-//             <Col md={12} xs={12}>
-//               <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-//                 <Form.Label>Example textarea</Form.Label>
-//                 <Form.Control
-//                   as="textarea"
-//                   rows={5}
-//                   name="message"
-//                   value={formData.message}
-//                   onChange={handleChange}
-//                 />
-//               </Form.Group>
-//             </Col>
-//           </Row>
-//           <Row>
-//             <Col md={4} xs={12}>
-//               <button className="send px-4 py-3" disabled={sending}>
-//                 {sending ? 'Sending...' : 'Send Message'}
-//               </button>
-//             </Col>
-//           </Row>
-//         </Form>
-//       </div>
-//       <Row>
-//         <Col md={4} xs={12}>
-//         <img src={image} alt="image"  width="100%"
-//         height="100%"/>
-//         </Col>
-//            <Col md={1} xs={12}>
-//         </Col>
-//          <Col md={7} xs={12} className='text-start'>
-//          <h1 className='fw-semibold'>We'd love to talk about how we can work together.</h1>
-//          <p className='py-3'>Simply dummy text of the printing and typesetting industry. Lorem had ceased to 
-// been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.</p>
-// import { LuPhone } from 'react-icons/lu';
-
-// <div className="col-6 col-md-6 p-3 my-3 shadow-sm rounded d-flex align-items-center" style={{ backgroundColor: '#fff' }}>
-//   <span style={{ backgroundColor: '#F4F4F4', padding: '8px', borderRadius: '6px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-//     <LuPhone size={24} color="black" />
-//   </span>
-//   <span className="ms-3 fw-bold">+91 7305821333</span>
-// </div>
-// <div className="gmap">
- 
-//    <iframe
-//         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.3844001187704!2d80.21240307454717!3d13.011175614010478!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5267245a3ed16f%3A0xe21d6ed23645a3c1!2sRainbow%20Media!5e0!3m2!1sen!2sin!4v1749536959456!5m2!1sen!2sin"
-//         width="100%"
-//         height="100%"
-//         style={{ border: 0}}
-//         allowFullScreen=""
-//         loading="lazy"
-//         referrerPolicy="no-referrer-when-downgrade"
-//         title="Google Map"
-//       />
-// </div>
 
 
-//         </Col>
-//       </Row>
-//     </Container>
-//   );
-// }
 
-// export default Contact;
-
-
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col, Container, Form, Row } from 'react-bootstrap';
 import * as emailjs from '@emailjs/browser'; 
 import image from '../assets/images/photo.webp';
 import { LuPhone } from "react-icons/lu";
 import { BsGeoAltFill, BsEnvelopeFill, BsTelephoneFill } from 'react-icons/bs';
+emailjs.init('JGE8YzhrZvPoiqxKt');
+
 function Contact() {
   const [formData, setFormData] = useState({
     name: '',
@@ -204,7 +20,9 @@ function Contact() {
   const [validated, setValidated] = useState(false);
   const [sending, setSending] = useState(false);
   const [sent, setSent] = useState(false);
-
+useEffect(() => {
+  emailjs.init('JGE8YzhrZvPoiqxKt');
+}, []);
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
@@ -226,9 +44,9 @@ function Contact() {
     emailjs
       .send(
         'service_ogarmcr',
-        'template_nhoy05f',
+        'template_9dw56kg',
         formData,
-        'cTHXHzBfD_w8wvixJ'
+        'JGE8YzhrZvPoiqxKt'
       )
       .then(() => {
         setSent(true);
@@ -249,7 +67,7 @@ function Contact() {
     <Container className="text-center my-5">
       <h1>Contact us</h1>
       <div className="form my-5">
-        <Form noValidate validated={validated} onSubmit={handleSubmit} className="text-start">
+       
          <Row className="my-5 align-items-stretch">
   {/* Left Side - Contact Info */}
    <Col md={4} xs={12} className="mb-4 ">
@@ -267,7 +85,7 @@ function Contact() {
       {/* Email */}
       <div className="mb-4 text-center">
            <BsEnvelopeFill className="text-success fs-2 mb-2" />
-        <p className="mb-0 fw-semibold">arihainfoods@gmail.com</p>
+        <p className="mb-0 fw-semibold">wecare@arihafoods.com</p>
       </div>
 
       <hr />
@@ -275,7 +93,7 @@ function Contact() {
       {/* Phone */}
       <div className="text-center">
             <BsTelephoneFill className="text-success fs-2 mb-2" />
-        <p className="mb-0 fw-semibold">+91 98844 55605</p>
+        <p className="mb-0 fw-semibold">+91 9884455605</p>
       </div>
     </div>
   </Col>
@@ -355,7 +173,7 @@ function Contact() {
     </div>
   </Col>
 </Row>
-</Form>
+
      </div>
     </Container>
   );
